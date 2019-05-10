b460bc3e21976f06c3e6f6c2eaff3ecc45f8509c
Neddy AP <45940569+neddy1298@users.noreply.github.com>
2019-05-10 15:52:08 +0700

    Add some host & port

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from flaskblog import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True,host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=8000,debug=True)
