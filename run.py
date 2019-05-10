7ed8ad7d38a800c2bd7bd592a90a35942e1a0e21
Neddy <neddy1298@gmail.com>
2019-05-10 15:52:56 +0700

    Add Some Port

--- run.py
+++ run.py
@@ -2,8 +2,4 @@ from flaskblog import create_app
 app = create_app()
 
 if __name__ == '__main__':
-<<<<<<< HEAD
     app.run(port=8000,debug=True)
-=======
-    app.run(host="0.0.0.0", port=8000,debug=True)
->>>>>>> b460bc3e21976f06c3e6f6c2eaff3ecc45f8509c
